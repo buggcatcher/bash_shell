38d8ba878e0adf77deb4d8f711f1f2114aed0b98
Bug Catcher <163436093+buggcatcher@users.noreply.github.com>
2025-06-11 13:59:45 -0400

    brr sksk

---
+++ to_do/minishell VaMa_V2/minishell.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vloddo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/28 12:50:13 by vloddo            #+#    #+#             */
+/*   Updated: 2025/05/28 12:50:15 by vloddo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+bool	exit_status = false; //Lo aggiorni dopo l'esecuzione di un comando, per esempio:
+
+// int status;
+// waitpid(pid, &status, 0);
+// if (WIFEXITED(status))
+// 	exit_status = WEXITSTATUS(status);  // aggiorna il valore globale
+
+
+// funzione per testare che controlla i token (DA RIMUOVERE)
+void	ft_print_token(t_token *token)
+{
+	int	i;
+
+	i = 0;
+	while (token)
+	{
+		printf("Token[%d]: Type=%d, Value='%s'\n", i++, token->type, token->value);
+		token = token->next;
+	}
+}
+
+void	ft_print_nodes(t_node *node)
+{
+	int i;
+	int index = 0;
+	t_redir *redir;
+
+	while (node)
+	{
+		printf("Comando %d:\n", ++index);
+		if (node->argv)
+		{
+			for (i = 0; node->argv[i]; i++)
+				printf("  argv[%d]: %s\n", i, node->argv[i]);
+		}
+		else
+		{
+			printf("  (nessun argomento)\n");
+		}
+		redir = node->redirs;
+		while (redir)
+		{
+			 char *symbol;
+			switch (redir->type)
+			{
+				case TK_REDIR_IN_2:
+					symbol = "<";
+					break;
+				case TK_REDIR_OUT_3:
+					symbol = ">";
+					break;
+				case TK_REDIR_APPEND_4:
+					symbol = ">>";
+					break;
+				case TK_HEREDOC_5:
+					symbol = "<<";
+					break;
+				default:
+					symbol = "(tipo sconosciuto)";
+					break;
+			}
+			printf("  Redirect: %s %s (fd: %d)\n", symbol, redir->filename, redir->fd);
+			redir = redir->next;
+		}
+		node = node->next;
+		if (node)
+			printf("  [Pipe ->]\n");
+	}
+}
+
+
+int	main(int arc, char **argv, char **envp)
+{
+	(void)arc;
+	(void)argv;
+
+	char	*input;
+	t_token	*token;
+	t_node	*node;
+	t_env *env = init_env_from_system(envp); // Inizializza con variabili di sistema
+	int status = 0;
+
+	token = NULL;
+	node = NULL;
+	while (1)
+	{
+		input = readline("minishell$ "); // Se l’utente digita qualcosa e preme Invio, readline() ritorna un puntatore a una stringa allocata dinamicamente contenente quel testo (senza il carattere \n).
+		if (!input) // Se l’utente preme Ctrl-D a inizio riga, readline() ritorna NULL.
+			break; // il programma termina
+		if (input && *input) // se input != NULL e non vuota
+			add_history(input); // aggiungere una riga di testo alla cronologia dei comandi.
+		token = ft_tokenize(token, input);
+		if (!token) // conta i token 
+		{
+			ft_free_token(token);
+			rl_clear_history();
+			exit(1);
+		}
+		ft_print_token(token); // funzione per testare che controlla i token (DA RIMUOVERE)
+		ft_check_syntax(token);
+		node = ft_node(token);
+		ft_print_nodes(node); // funzione per testare che controlla i nodi (DA RIMUOVERE)
+		executor_loop(node, &env, &status);
+		//debug_print_env(env);
+		debug_status(status);
+		free(input);
+		ft_free_token(token);
+		ft_free_nodes(node);
+	}
+	rl_clear_history(); // NON prende parametri.
+	return (0);
+}
