e171e182353da4c709d06ae8209efdb09d690da2
valoddo <161822352+valoddo@users.noreply.github.com>
2025-06-20 22:24:38 +0200

    Add files via upload

---
+++ minishell_VaMa_V4_4/tokenize_mini.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vloddo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/28 12:50:13 by vloddo            #+#    #+#             */
+/*   Updated: 2025/05/28 12:50:15 by vloddo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_token	*ft_tokenize(t_shell_state *state, t_token *token, char *input)
+{
+	t_token	*head;
+	t_token	*tail;
+	t_token	*new;
+
+	head = NULL;
+	tail = NULL;
+	while (*input)
+	{
+		while (*input == ' ')
+			input++;
+		if (*input == '\0')
+			break ;
+		if (!ft_get_token(state, token, &input, &new)) // MODIFICATO: passato state a ft_get_token NUOVO
+			return (NULL);
+		if (!head)
+			head = new;
+		else
+			tail->next = new;
+		tail = new;
+	}
+	return (head);
+}
+
+t_token	*ft_get_token(t_shell_state *state, t_token *token, char **input, t_token **new)
+{
+	*new = NULL;
+	if (**input == '|')
+		*new = ft_pipe(new, input);
+	else if (**input == '<')
+		*new = ft_redher(new, input);
+	else if (**input == '>')
+		*new = ft_redred(new, input);
+	else if (**input == '\'')
+		*new = ft_squote(token, new, input); // MODIFICATO NUOVO
+	else if (**input == '"')
+		*new = ft_dquote(state, token, new, input); // MODIFICATO NUOVO
+	else
+		*new = ft_word(state, new, input); // MODIFICATO NUOVO
+	return (*new);
+}
+
+t_token *ft_word(t_shell_state *state, t_token **new, char **input) // MODIFICATO NUOVO
+{
+	char	*start;
+	char		*buffer;
+	int			var;
+
+	start = *input;
+	buffer = NULL;
+	var = 1;
+	while (**input && **input != ' ' && **input != '|' && **input != '<' && **input != '>' && 
+			**input != '\'' && **input != '"')  
+	{
+		if (var != 2)
+			var = ft_check_var(input);			
+		buffer = ft_create_var(buffer, input, state);  // MODIFICATO NUOVO
+	}
+	if (var == 1)
+		*new = ft_create_token(TK_WORD_0, start, *input - start);
+	else if (var == 2)
+		*new = ft_create_token(TK_DOLLAR_8, buffer, ft_strlen_v(buffer));
+	free(buffer);
+	return (*new);
+}
+
+t_token	*ft_create_token(t_token_type type, char *start, int len)
+{
+	t_token	*new;
+
+	new = malloc(sizeof(t_token));
+	if (!new)
+		return (NULL);
+	new->type = type;
+	new->value = ft_strndup(start, len);
+	new->next = NULL;
+	return (new);
+}
