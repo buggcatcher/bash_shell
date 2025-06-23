e957d0abe85f39983cfbc15b6b33b58132fd2652
Bug Catcher <163436093+buggcatcher@users.noreply.github.com>
2025-06-23 10:13:41 -0400

    🐱‍💻

---
+++ add me/btin.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vloddo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/28 12:50:13 by vloddo            #+#    #+#             */
+/*   Updated: 2025/05/28 12:50:15 by vloddo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	is_parent_builtin(char *cmd)
+{
+	if (!cmd)
+		return (0);
+	return (!ft_strcmp(cmd, "cd") || \
+			!ft_strcmp(cmd, "export") || \
+			!ft_strcmp(cmd, "unset") || \
+			!ft_strcmp(cmd, "exit"));
+}
+
+int	is_child_builtin(char *cmd)
+{
+	if (!cmd)
+		return (0);
+	return (!ft_strcmp(cmd, "echo") || \
+			!ft_strcmp(cmd, "pwd") || \
+			!ft_strcmp(cmd, "env"));
+}
+
+int	should_execute_in_parent(t_node *node)
+{
+	return (is_parent_builtin(node->argv[0]) && !node->next);
+}
+
+int	is_builtin(char *cmd)
+{
+	if (!cmd)
+		return (0);
+	return (is_parent_builtin(cmd) || is_child_builtin(cmd));
+}
+
+
---
+++ add me/exe_btin.c
@@ -0,0 +1,71 @@
+#include "minishell.h"
+
+int	exe_pwd(char **args, t_env **env)
+{
+	char	cwd[PATH_MAX + 1];
+
+	(void)args;
+	(void)env;
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+	{
+		printf("%s\n", cwd);
+		return (0);
+	}
+	perror("pwd");
+	return (1);
+}
+
+int	exe_unset(char **args, t_env **env)
+{
+	int	i;
+	int	exit_status;
+
+	i = 1;
+	exit_status = 0;
+	if (!args[1])
+		return (write(2, "unset: not enough arguments\n", 28), 1);
+	while (args[i])
+	{
+		if (remove_env_node(env, args[i]) != 0)
+			printf("nodo non rimosso\n");
+		i++;
+	}
+	return (exit_status);
+}
+
+int	exe_env(t_env *env)
+{
+	while (env)
+	{
+		if (env->exported)
+			printf("%s=%s\n", env->key, env->value);
+		env = env->next;
+	}
+	return (0);
+}
+
+int	exe_echo(char **args)
+{
+	int	i;
+	int	no_newline;
+
+	i = 1;
+	no_newline = 0;
+	if (args[1] && ft_strcmp(args[1], "-n") == 0)
+	{
+		no_newline = 1;
+		i = 2;
+	}
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		if (args[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (!no_newline)
+		printf("\n");
+	return (0);
+}
+
+
