a609527a186f1153ee25ae84a31980d03743f125
Bug Catcher <163436093+buggcatcher@users.noreply.github.com>
2025-06-28 14:57:17 -0400

    fixed export

---
+++ export/Minishell_V4_7/free_mini.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_mini.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/28 12:50:13 by vloddo            #+#    #+#             */
+/*   Updated: 2025/06/26 20:18:11 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_free_token(t_token *token)
+{
+	t_token	*tmp;
+
+	while (token)
+	{
+		tmp = token;
+		token = token->next;
+		free(tmp->value);
+		free(tmp);
+	}
+}
+
+t_node	*ft_free_nodes(t_node *head)
+{
+	t_node	*tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		ft_free_argv(tmp->argv);
+		ft_free_redirs(tmp->redirs);
+		free(tmp);
+	}
+	return (NULL);
+}
+
+void	ft_free_argv(char **argv)
+{
+	int	i;
+
+	if (!argv)
+		return ;
+	i = 0;
+	while (argv[i])
+	{
+		free(argv[i]);
+		i++;
+	}
+	free(argv);
+}
+
+void	ft_free_redirs(t_redir *redir)
+{
+	t_redir	*next_redir;
+
+	while (redir)
+	{
+		next_redir = redir->next;
+		free(redir->filename);
+		free(redir);
+		redir = next_redir;
+	}
+}
+
+void	free_array(char **arr)
+{
+	int	i;
+
+	i = 0;
+	if (!arr)
+		return ;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
